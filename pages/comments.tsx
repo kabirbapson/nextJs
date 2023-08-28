a06d0e0e81a0a786ee0be18dab6a6e2c8ac873d2
Bappi Kabir <kabirbapson@gmail.com>
2023-08-28 23:52:27 +0100

    feat: finishing touches

--- pages/comments.tsx
+++ pages/comments.tsx
@@ -24,6 +24,7 @@ export default function Comments() {
         "Content-Type": "application/json",
       },
     });
+    
     const mainResponse = await response.json();
   };
   const handleDelete = async (commentId: number) => {
